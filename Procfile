db27ba70881dace7c16437d9b5602d30b187a9c7
Sonya <isaisa553@gmail.com>
2022-04-20 00:48:20 +0300

    Diploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn newstask.wsgi --log-file -
--- liis/settings.py
+++ liis/settings.py
@@ -11,6 +11,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-44%jkll_@g2_o7=fwz)7sjj*!^9yqt#ydadd5smu^&p0rx*3&^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,6 +82,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
